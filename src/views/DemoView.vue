fdaf3b1b3b539b784abb5e78be1ce87dc65eb03a
Jesús García Crespo <jesus@sevein.com>
2022-08-25 17:53:25 +0200

    Update demo page to consume store

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,15 +1,36 @@
+<script setup lang="ts">
+import { usePackageStore } from "../stores/package";
+import { useAsyncState } from "@vueuse/core";
+
+const packageStore = usePackageStore();
+
+const { execute, error } = useAsyncState(() => {
+  return packageStore.fetchPackages();
+}, null);
+
+const retry = () => {
+  if (execute) execute();
+};
+</script>
+
 <template>
   <div class="demo">
-    <h1>This is a demo.</h1>
+    <h1>Packages</h1>
+
+    <div v-if="error">
+      An error occured!
+      <pre>{{ error }}</pre>
+      <button class="btn btn-danger" @click="retry">Retry</button>
+    </div>
+
+    <ul v-if="packageStore.packages">
+      <li v-for="pkg in packageStore.packages" :key="pkg.id">
+        Package {{ pkg.id }}
+      </li>
+    </ul>
+
+    <div v-if="!packageStore.packages.length">
+      Not ready yet!
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .demo {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
