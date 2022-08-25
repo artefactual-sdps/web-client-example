cb0a2e5fdc9faed70ef826f60b615d4b3a810c52
Jesús García Crespo <jesus@sevein.com>
2022-08-25 17:23:59 +0200

    Add demo page placeholder

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/demo',
+      name: 'demo',
+      component: () => import('../views/DemoView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
---
+++ src/views/DemoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo">
+    <h1>This is a demo.</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .demo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
